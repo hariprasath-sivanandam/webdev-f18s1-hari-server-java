1e44ac3453a197b6ca61cbe97c256fcfe2a9b7d6
hariprasath <sivanandam.h@husky.neu.edu>
2018-09-17 13:22:27 -0400

    moving all the files from static pages to outside

---
+++ src/main/java/edu/northeastern/cs5610/StaticPagesApplication.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5610;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticPagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticPagesApplication.class, args);
+	}
+}
